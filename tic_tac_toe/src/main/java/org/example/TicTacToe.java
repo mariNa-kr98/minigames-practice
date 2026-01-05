2e54058eaa54d4405d598938e249d2faa3f94036
Marina Krommyda <kmarikro@gmail.com>
2026-01-05 12:17:41 +0200

    Update TicTacToe.java

--- tic_tac_toe/src/main/java/org/example/TicTacToe.java
+++ tic_tac_toe/src/main/java/org/example/TicTacToe.java
@@ -13,20 +13,25 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class TicTacToe extends Application {
-    private int clickCount = 0;
+    
+    char turn = 'X';
+    int scoreX = 0;
+    int scoreO = 0;
+    
     private VBox createContent() {
-        Button btnReset = new Button("Reset");
+        Button btnReset = new Button("New game");
         Button[][] buttonBoard = new Button[3][3];
         char[][] board = new char[3][3];
         AtomicInteger counter1 = new AtomicInteger();
         AtomicInteger counter2 = new AtomicInteger();
-        String player1 = "player 1";
-        String player2 = "player 2";
+        String player1 = "player X";
+        String player2 = "player O";
 
         btnReset.setPrefSize(200, 100);
         btnReset.setFont(new Font(25));
@@ -37,44 +42,10 @@ public class TicTacToe extends Application {
         playerOne.setFont(new Font(20));
         Label playerTwo = new Label(String.valueOf(player2));
         playerTwo.setFont(new Font(20));
-        Label scoreLabel1 = new Label(String.valueOf(counter1.get()));
+        Label scoreLabel1 = new Label(String.valueOf(scoreX));
         scoreLabel1.setFont(new Font(20));
-        Label scoreLabel2 = new Label(String.valueOf(counter2.get()));
+        Label scoreLabel2 = new Label(String.valueOf(scoreO));
         scoreLabel2.setFont(new Font(20));
-
-        Button increaseScore1 = new Button();
-        Button decreaseScore1 = new Button();
-        Button increaseScore2 = new Button();
-        Button decreaseScore2 = new Button();
-        increaseScore1.setText("+");
-        increaseScore1.setFont(new Font(20));
-        increaseScore1.setPrefSize(40, 40);
-        decreaseScore1.setPrefSize(40, 40);
-        decreaseScore1.setFont(new Font(20));
-        decreaseScore1.setText("-");
-        increaseScore2.setText("+");
-        increaseScore2.setFont(new Font(20));
-        increaseScore2.setPrefSize(40, 40);
-        decreaseScore2.setPrefSize(40, 40);
-        decreaseScore2.setFont(new Font(20));
-        decreaseScore2.setText("-");
-
-        increaseScore1.setOnAction(e -> {
-            counter1.getAndIncrement();
-            scoreLabel1.setText(String.valueOf(counter1.get()));
-        });
-        decreaseScore1.setOnAction(e -> {
-            counter1.decrementAndGet();
-            scoreLabel1.setText(String.valueOf(counter1.get()));
-        });
-        increaseScore2.setOnAction(e -> {
-            counter2.getAndIncrement();
-            scoreLabel2.setText(String.valueOf(counter2.get()));
-        });
-        decreaseScore2.setOnAction(e -> {
-            counter2.decrementAndGet();
-            scoreLabel2.setText(String.valueOf(counter2.get()));
-        });
 
         Button btnResetScore = new Button( "Reset Score");
         btnResetScore.setPrefSize(170, 50);
@@ -82,15 +53,15 @@ public class TicTacToe extends Application {
         btnResetScore.setBackground(new Background(new BackgroundFill(Color.ANTIQUEWHITE, null, null)));
 
         btnResetScore.setOnAction(e -> {
-            counter1.set(0);
-            scoreLabel1.setText(String.valueOf(counter1.get()));
-            counter2.set(0);
-            scoreLabel2.setText(String.valueOf(counter2.get()));
+            scoreX = 0;
+            scoreLabel1.setText(Integer.toString(scoreX));
+            scoreO = 0;
+            scoreLabel2.setText(Integer.toString(scoreO));
         });
 
-        HBox score1 = new HBox(10, playerOne, increaseScore1, decreaseScore1, scoreLabel1);
+        HBox score1 = new HBox(10, playerOne, scoreLabel1);
         score1.setAlignment(Pos.CENTER);
-        HBox score2 = new HBox(10, playerTwo, increaseScore2, decreaseScore2, scoreLabel2);
+        HBox score2 = new HBox(10, playerTwo, scoreLabel2);
         score2.setAlignment(Pos.CENTER);
         HBox resetScore = new HBox(10, btnResetScore);
         resetScore.setAlignment(Pos.CENTER);
@@ -99,6 +70,9 @@ public class TicTacToe extends Application {
         gp.setAlignment(Pos.CENTER);
 
         btnReset.setOnMouseClicked(e -> {
+            playerOne.setFont(Font.font(null, FontWeight.BOLD, 36));
+            playerTwo.setFont(Font.font(null, FontWeight.NORMAL, 20));
+            turn = 'X';
             for (javafx.scene.Node node : gp.getChildren()) {
                 if (node instanceof Button b) {
                     b.setText("");
@@ -131,15 +105,20 @@ public class TicTacToe extends Application {
 
                     if (board[r][c] != '\0') return;
 
-                    if (clickCount % 2 == 0) {
+                    if (turn == 'X') {
                         button.setText("X");
                         board[r][c] = 'X';
+                        playerTwo.setFont(Font.font(null, FontWeight.BOLD, 36));
+                        playerOne.setFont(Font.font(null, FontWeight.NORMAL, 20));
+                        turn = 'O';
                     }
                     else {
                         button.setText("O");
                         board[r][c] = 'O';
+                        playerOne.setFont(Font.font(null, FontWeight.BOLD, 36));
+                        playerTwo.setFont(Font.font(null, FontWeight.NORMAL, 20));
+                        turn = 'X';
                     }
-                    clickCount++;
 
                     if(checkWin(board, buttonBoard)){
                         for (int i = 0; i < 3; i++) {
@@ -171,7 +150,11 @@ public class TicTacToe extends Application {
         stage.show();
     }
 
-    private boolean checkWin(char[][] board, Button[][] buttonBoard) {
+    private boolean checkWin(char[][] board, Button[][] buttonBoard, Label scorelabel1, Label scorelabel2) {
+
+        boolean winFound = false;
+        char playerWinFound = ' ';
+        
         for (int i = 0; i < 3; i++) {
             //row
             if (board[i][0] != '\0' && board[i][0] == board[i][1] && board[i][1] == board[i][2]) {
@@ -181,7 +164,8 @@ public class TicTacToe extends Application {
                 buttonBoard[i][1].setDisable(true);
                 buttonBoard[i][2].setBackground(new Background(new BackgroundFill(Color.GRAY, null, null)));
                 buttonBoard[i][2].setDisable(true);
-                return true;
+                winFound = true;
+                playerWinFound = board[i][0];
             } //col
             if (board[0][i] != '\0' && board[0][i] == board[1][i] && board[1][i] == board[2][i]) {
                 buttonBoard[0][i].setBackground(new Background(new BackgroundFill(Color.GRAY, null, null)));
@@ -190,7 +174,8 @@ public class TicTacToe extends Application {
                 buttonBoard[1][i].setDisable(true);
                 buttonBoard[2][i].setBackground(new Background(new BackgroundFill(Color.GRAY, null, null)));
                 buttonBoard[2][i].setDisable(true);
-                return true;
+                winFound = true;
+                playerWinFound = board[0][i];
             }//diagonal
             if (board[0][0] != '\0' && board[0][0] == board[1][1] && board[1][1] == board[2][2]) {
                 buttonBoard[0][0].setBackground(new Background(new BackgroundFill(Color.GRAY, null, null)));
@@ -199,7 +184,8 @@ public class TicTacToe extends Application {
                 buttonBoard[1][1].setDisable(true);
                 buttonBoard[2][2].setBackground(new Background(new BackgroundFill(Color.GRAY, null, null)));
                 buttonBoard[2][2].setDisable(true);
-                return true;
+                winFound = true;
+                playerWinFound = board[0][0];
             } //diagonal
             if (board[0][2] != '\0' && board[0][2] == board[1][1] && board[1][1] == board[2][0]) {
                 buttonBoard[0][2].setBackground(new Background(new BackgroundFill(Color.GRAY, null, null)));
@@ -208,9 +194,18 @@ public class TicTacToe extends Application {
                 buttonBoard[1][1].setDisable(true);
                 buttonBoard[2][0].setBackground(new Background(new BackgroundFill(Color.GRAY, null, null)));
                 buttonBoard[2][0].setDisable(true);
-                return true;
+                winFound = true;
+                playerWinFound = board[0][2];
             }
         }
-        return false;
+        if (playerWinFound == 'X'){
+            scoreX += 1;
+            scorelabel1.setText(Integer.toString(scoreX));
+        }
+        else if (playerWinFound == 'O'){
+            scoreO += 1;
+            scorelabel2.setText(Integer.toString(scoreO));
+        }
+        return winFound;
     }
 }
